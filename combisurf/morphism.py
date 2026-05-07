3461fb76462f014818a09e47fa631ece97ab976d
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2026-05-07 11:50:44 +0200

    redesign morphism (since they are not defined on half-edges)

--- combisurf/morphism.py
+++ combisurf/morphism.py
@@ -28,38 +28,25 @@ from combisurf.word import word_init, word_reduce
 from combisurf.oriented_map import OrientedMap
 
 class OrientedMapMorphism:
-    def __init__(self, domain, codomain):
-        self._domain = domain
-        self._codomain = codomain
+    r"""
+    Morphism between oriented maps.
 
-    def domain(self):
-        return self._domain
+    A morphism is a partial maps between walks in the domain to walks in the
+    codomain. The domain must be a subset of walks stable under concatenation
+    and the map should respect concatenation. More precisely, if walks
+    ``u`` and ``v`` belongs to the domain and for a morphism ``f`` both
+    ``f(u)`` and ``f(v)`` are defined then we impose that ``f(uv)`` exists
+    and assume that ``f(u) f(v) = f(uv)``.
+    """
+    def __init__(self, domain_map, codomain_map):
+        self._domain_map = domain_map
+        self._codomain_map = codomain_map
 
-    def codomain(self):
-        return self._codomain
+    def domain_map(self):
+        return self._domain_map
 
-    def _image(self, h, check=True):
-        r"""
-        Return the image of the half-edge ``h`` as an array of integers.
-        """
-        raise NotImplementedError
-
-    def __repr__(self):
-        return f"OrientedMapMorphism({self._domain}, {self._codomain}, {[self._image(h) for h in self._domain.half_edges()]})"
-
-    # TODO: should we have an option to remove spur in domain/codomain?
-    def __call__(self, w, reduce=False, check=True):
-        if check:
-            w = word_init(w)
-        ans = array("i")
-        if reduce:
-            w = word_reduce(w)
-            for h in w:
-                word_free_group_mul_inplace(ans, self._image(h))
-        else:
-            for h in w:
-                ans += self._image(h)
-        return ans
+    def codomain_map(self):
+        return self._codomain_map
 
 
 class OrientedMapMorphism_list(OrientedMapMorphism):
@@ -69,19 +56,41 @@ class OrientedMapMorphism_list(OrientedMapMorphism):
         sage: from combisurf import OrientedMap
         sage: m = OrientedMap(fp="(0,1,~0,~1)")
         sage: f = m.hom([[0,2], [3,1], [0], [1]])
+        sage: f
+        OrientedMapMorphism(OrientedMap("(0,1,~0,~1)", "(0,1,~0,~1)"), OrientedMap("(0,1,~0,~1)", "(0,1,~0,~1)"), [[0, 2], [3, 1], [0], [1]])
         sage: f([0, 2, 0])
         array('i', [0, 2, 0, 0, 2])
         sage: f([0, 1], reduce=True)
         array('i')
     """
-    def __init__(self, domain, codomain, images):
-        OrientedMapMorphism.__init__(self, domain, codomain)
+    def __init__(self, domain_map, codomain_map, images):
+        OrientedMapMorphism.__init__(self, domain_map, codomain_map)
         images = list(map(word_init, images))
-        if len(images) != len(self._domain._vp):
+        if len(images) != len(self._domain_map._vp):
             raise ValueError("wrong data to specify images")
         self._images = images
 
-    def _image(self, h, check=True):
+    def __repr__(self):
+        return f"OrientedMapMorphism({self._domain_map}, {self._codomain_map}, {[list(self._half_edge_image(h)) for h in self._domain_map.half_edges()]})"
+
+    def _half_edge_image(self, h, check=True):
         if check:
-            h = self.domain()._check_half_edge(h)
+            h = self._domain_map._check_half_edge(h)
         return self._images[h]
+
+    def _array_image(self, w, reduce=False):
+        ans = array("i")
+        if reduce:
+            w = word_reduce(w)
+            for h in w:
+                word_free_group_mul_inplace(ans, self._half_edge_image(h))
+        else:
+            for h in w:
+                ans += self._half_edge_image(h)
+        return ans
+
+    # TODO: make __call__ take a walk on the domain as input
+    def __call__(self, w, reduce=False, check=True):
+        if check:
+            w = word_init(w)
+        return self._array_image(w, reduce)
